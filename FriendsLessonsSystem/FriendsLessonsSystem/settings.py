a00a9aee6c138f6c2dc6ad092d29838abbd97c75
Giorgetti, Valentin <valentingiorgetti@hotmail.com>
2022-12-30 20:04:31 -0300

    Middleware para añadir header a todos los requests

--- FriendsLessonsSystem/FriendsLessonsSystem/settings.py
+++ FriendsLessonsSystem/FriendsLessonsSystem/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'FriendsLessonsAPI.middleware.HeaderMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
