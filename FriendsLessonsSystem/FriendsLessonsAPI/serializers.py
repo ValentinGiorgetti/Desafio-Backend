c29a814f1d3db4e3abf09cd946b68c0189853e2b
Giorgetti, Valentin <valentingiorgetti@hotmail.com>
2022-12-30 20:01:36 -0300

    Implementación de serializadores

---
+++ FriendsLessonsSystem/FriendsLessonsAPI/serializers.py
@@ -0,0 +1,40 @@
+from rest_framework import serializers
+
+from .models import User, Course, Enrollment
+
+class BaseUserSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = User
+        exclude = ['friends']
+
+class UserFriends(serializers.ModelSerializer):
+
+    friends = BaseUserSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = User
+        fields = '__all__'
+
+class BaseCourseSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Course
+        fields = '__all__'
+
+class BaseEnrollmentSerializer(serializers.ModelSerializer):
+
+    course = BaseCourseSerializer(read_only=True)
+
+    class Meta:
+        model = Enrollment
+        exclude = ['id', 'user']
+
+class UserCourses(BaseUserSerializer):
+
+    courses = serializers.SerializerMethodField()
+
+    def get_courses(self, obj):
+        students = obj.enrollment_set.all()
+        serializer = BaseEnrollmentSerializer(students, many=True)
+        return serializer.data
